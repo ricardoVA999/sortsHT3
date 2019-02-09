0c05a64629bf74b6c2a74afc87842d00b0b6b707
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2019-02-08 21:20:27 -0600

    Add files via upload

--- sortsHT3/src/Main.java
+++ sortsHT3/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Nombre del archivo: sortsHT3
  * Programa que ordena una lista de numeros de 0
@@ -7,14 +9,82 @@ import java.util.Random;
  * @author Ricardo Valenzuela 18762   Jose Block 18935
  */
 public class Main {
+    private static Scanner input;
     public static void main(String[] args){
-        compare[] listaNumeros = new compare[3000];
-        compare[] listaOrdenada = new compare[3000];
-        WriteFile("./numeros.txt");
-        listaNumeros = ReadFile("./numeros.txt");
+        input = new Scanner(System.in);
         sorts mysorts = new sorts();
-        listaOrdenada= mysorts.bubble(listaNumeros);
-        WriteSortFile("./SortedNumbers.txt", listaOrdenada);
+        System.out.println("Cuantos numeros desea ordenar: ");
+        String numero = (input.next());
+        try{
+            int num = Integer.parseInt(numero);
+            if(num<=3000&&num>=10){
+                WriteFile("./numeros.txt", num);
+                compare[] listaNumeros = new compare[num];
+                compare[] listaOrdenada = new compare[num];
+                listaNumeros = ReadFile("./numeros.txt", num);
+                for(int i=0; i<=listaNumeros.length-1; i++){
+                    System.out.print(listaNumeros[i].getX()+" ");
+                }
+                System.out.println(""
+                        +"\n"
+                        + "Sorts\n"
+                        + "1. Quick\n"
+                        + "2. Gnome\n"
+                        + "3. Merge\n"
+                        + "4. Bubble\n"
+                        + "5. Radix\n"
+                        + "");
+                System.out.println("Seleccione una opcion: ");
+                String election = input.next();
+                listaNumeros = ReadFile("./numeros.txt", num);
+                switch (election){
+                    case "1":
+                        listaOrdenada=mysorts.quickSort(listaNumeros,0,listaNumeros.length-1);
+                        WriteSortFile("./SortedNumbers.txt", listaOrdenada);
+                        for(compare i: listaOrdenada){
+                            System.out.print(i.getX()+" ");
+                        }
+                        break;
+                    case"2":
+                        listaOrdenada=mysorts.gnome(listaNumeros,listaNumeros.length);
+                        WriteSortFile("./SortedNumbers.txt", listaOrdenada);
+                        for(compare i: listaOrdenada){
+                            System.out.print(i.getX()+" ");
+                        }
+                        break;
+                    case"3":
+                        listaOrdenada=mysorts.mergeSort(listaNumeros,0,listaNumeros.length-1);
+                        WriteSortFile("./SortedNumbers.txt", listaOrdenada);
+                        for(compare i: listaOrdenada){
+                            System.out.print(i.getX()+" ");
+                        }
+                        break;
+                    case"4":
+                        listaOrdenada=mysorts.bubble(listaNumeros);
+                        WriteSortFile("./SortedNumbers.txt", listaOrdenada);
+                        for(compare i: listaOrdenada){
+                            System.out.print(i.getX()+" ");
+                        }
+                        break;
+                    case"5":
+                        listaOrdenada=mysorts.radixsort(listaNumeros,listaNumeros.length);
+                        WriteSortFile("./SortedNumbers.txt", listaOrdenada);
+                        for(compare i: listaOrdenada){
+                            System.out.print(i.getX()+" ");
+                        }
+                        break;
+                    default:
+                        System.out.println("Esa opcion no existe");;
+                        break;
+
+                }
+            }else{
+                System.out.println("Su cantidad ingresada no es permitida");
+            }
+
+        }catch(NumberFormatException ex){
+            System.out.println("No es un número");
+        }
     }
 
 
@@ -23,9 +93,9 @@ public class Main {
      * @param fileName, nombre del archivo txt
      * @return una lista de tipo compare
      */
-    private static compare[] ReadFile(String fileName){
+    private static compare[] ReadFile(String fileName, int numero){
 
-        compare[] textNumbers =  new compare[3000];
+        compare[] textNumbers =  new compare[numero];
 
         try {
             FileReader fileReader = new FileReader(fileName);
@@ -50,7 +120,7 @@ public class Main {
      * Escribe en un archivo
      * @param fileName, nombre del archivo
      */
-    private static void WriteFile(String fileName){
+    private static void WriteFile(String fileName, int cantidad){
         try{
 
             File text = new File(fileName);
@@ -58,10 +128,12 @@ public class Main {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 
             Random randomNumbers = new Random();
-            for (int i = 0; i <= 2999; i++) {
+            for (int i = 0; i <= cantidad-1; i++) {
                 int num = randomNumbers.nextInt(3001);
                 bw.write("" + num );
-                bw.newLine();
+                if(i!=cantidad-1){
+                    bw.newLine();
+                }
             }
             bw.close();
         }
